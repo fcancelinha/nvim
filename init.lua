5b1e38422e0da9b5ab5129602c78ee86ef649617
Filipe Cancelinha <fcancelinha@protonmail.com>
2023-09-09 20:56:38 +0100

    feat: abstracts colors in preparation for colorscheme, adds simplified noice, minor plugin rework

--- init.lua
+++ init.lua
@@ -7,36 +7,4 @@ vim.cmd [[
 	colorscheme nord
 ]]
 
-local TelescopePrompt = {
-	TelescopeBorder = {
-		fg = "#88C0D0",
-	},
-	TelescopeTitle = {
-		fg = "#ECEFF4",
-	},
-	TelescopePreviewBorder = {
-		fg = "#88C0D0"
-	},
-	TelescopePromptBorder = {
-		fg = "#88C0D0"
-	},
-	TelescopeResultsBorder = {
-		fg = "#88C0D0"
-	},
-	TelescopeResultBorder = {
-		fg = "#88C0D0"
-	},
-	TelescopeSelectionBorder = {
-		fg = "#88C0D0"
-	},
-	NeoTreeFloatBorder = {
-		fg = "#88C0D0"
-	},
-	NeoTreeDirectoryIcon = {
-		fg = "#8FBCBB"
-	}
-}
-
-for hl, col in pairs(TelescopePrompt) do
-	vim.api.nvim_set_hl(0, hl, col)
-end
+require('config.highlights')
