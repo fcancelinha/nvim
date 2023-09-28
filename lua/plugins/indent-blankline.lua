1956a2b35cd053e1130a344b9641f9268ac3d2f8
Filipe Cancelinha <fcancelinha@pm.me>
2023-09-28 17:51:09 +0100

    fix: indentblankline

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -4,20 +4,51 @@ return {
 	config = function()
 		require("ibl").setup({
 			enabled = true,
+			debounce = 100,
+			viewport_buffer = {
+				min = 50,
+				max = 200,
+			},
 			indent = {
+				enabled = true,
 				char = "⁞",
 				tab_char = "⁞",
 				smart_indent_cap = false,
 			},
 			whitespace = {
+				enabled = true,
 				remove_blankline_trail = true
 			},
 			scope = {
+				enabled = true,
 				show_start = false,
 				show_end = false,
-				enabled = true,
+				injected_languages = false,
 				include = {
-					node_type = { "*" },
+					node_type = {
+						["*"] = {
+							"argument",
+							"arguments",
+							"block",
+							"bracket",
+							"declaration",
+							"expression_list",
+							"field",
+							"for",
+							"func_literal",
+							"function",
+							"if",
+							"import",
+							"import_spec_list",
+							"list",
+							"return_statement",
+							"short_var_declaration",
+							"statement",
+							"switch_body",
+							"try",
+							"type",
+						},
+					},
 				},
 				exclude = {
 					filetypes = {
