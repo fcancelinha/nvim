8a83962498d89eccefc170790b35d71850b80f12
Filipe Cancelinha <filipe.cancelinha@siemens.com>
2024-07-20 20:16:42 +0100

    config: improvement

--- lua/plugins/barbecue.lua
+++ lua/plugins/barbecue.lua
@@ -1,31 +1,34 @@
 return {
-    "utilyre/barbecue.nvim",
-    version = "*",
-    dependencies = {
-        "SmiteshP/nvim-navic",
-        "nvim-tree/nvim-web-devicons", -- optional dependency
-    },
-    config = function()
-        require("barbecue").setup({
-            attach_navic = false,
-            theme = {
-                fileicon_lua = { bg = "#2e3440" }
-            }
-        })
+	"utilyre/barbecue.nvim",
+	version = "*",
+	dependencies = {
+		"SmiteshP/nvim-navic",
+		"nvim-tree/nvim-web-devicons", -- optional dependency
+	},
+	config = function()
+		require("barbecue").setup({
+			attach_navic = true,
+			create_autocmd = false,
+			theme = {
+				-- this highlight is used to override other highlights
+				-- you can take advantage of its `bg` and set a background throughout your winbar
+				-- (e.g. basename will look like this: { fg = "#c0caf5", bold = true })
+				normal = { fg = "#E5E9F0", bg = "#2E3440", bold = true, underdotted = true },
+			},
+		})
 
-        vim.api.nvim_create_autocmd({
-            "WinScrolled", -- or WinResized on NVIM-v0.9 and higher
-            "BufWinEnter",
-            "CursorHold",
-            "InsertLeave",
-
-            -- include this if you have set `show_modified` to `true`
-            -- "BufModifiedSet",
-        }, {
-            group = vim.api.nvim_create_augroup("barbecue.updater", {}),
-            callback = function()
-                require("barbecue.ui").update()
-            end,
-        })
-    end
+		vim.api.nvim_create_autocmd({
+			"WinScrolled", -- or WinResized on NVIM-v0.9 and higher
+			"BufWinEnter",
+			"CursorHold",
+			"InsertLeave",
+			-- include this if you have set `show_modified` to `true`
+			-- "BufModifiedSet",
+		}, {
+			group = vim.api.nvim_create_augroup("barbecue.updater", {}),
+			callback = function()
+				require("barbecue.ui").update()
+			end,
+		})
+	end
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,181 +1,168 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = {
-        "Isrothy/lualine-diagnostic-message",
-    },
-    config = function()
-        local colors = {
-            dark           = '#2E3440',
-            frostblue      = '#5E81Ac',
-            frostgreen     = '#8FBCBB',
-            frostlightblue = '#81A1C1',
-            frostturquoise = "#88C0D0",
-            green          = '#A3BE8C',
-            grey           = "#434C5E",
-            greydark       = "#3B4252",
-            orange         = '#D08770',
-            purple         = '#B48EAD',
-            red            = '#BF616A',
-            snowdark       = '#D8DEE9',
-            snowlight      = '#ECEFF4',
-            snowshade      = '#E5E9F0',
-            yellow         = '#EBCB8B',
-            special        = "#384356",
-        }
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		local colors = {
+			dark           = '#2E3440',
+			frostblue      = '#5E81Ac',
+			frostgreen     = '#8FBCBB',
+			frostlightblue = '#81A1C1',
+			frostturquoise = "#88C0D0",
+			green          = '#A3BE8C',
+			grey           = "#434C5E",
+			greydark       = "#3B4252",
+			orange         = '#D08770',
+			purple         = '#B48EAD',
+			red            = '#BF616A',
+			snowdark       = '#D8DEE9',
+			snowlight      = '#ECEFF4',
+			snowshade      = '#E5E9F0',
+			yellow         = '#EBCB8B',
+			special        = "#384356",
+		}
 
-        local custom_northern = require('lualine.themes.northern')
+		local custom_northern = require('lualine.themes.northern')
 
-        custom_northern.normal.c.bg = colors.dark
-        custom_northern.insert.c.bg = colors.dark
-        custom_northern.visual.c.bg = colors.dark
-        custom_northern.replace.c.bg = colors.dark
-        custom_northern.command.c.bg = colors.dark
+		custom_northern.normal.c.bg = colors.dark
+		custom_northern.insert.c.bg = colors.dark
+		custom_northern.visual.c.bg = colors.dark
+		custom_northern.replace.c.bg = colors.dark
+		custom_northern.command.c.bg = colors.dark
 
-        local function lsp_server()
-            local msg = 'No Active Lsp'
-            local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-            local clients = vim.lsp.get_active_clients()
-            if next(clients) == nil then
-                return msg
-            end
-            for _, client in ipairs(clients) do
-                local filetypes = client.config.filetypes
-                if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                    return client.name
-                end
-            end
-            return msg
-        end
+		local function lsp_server()
+			local msg = ''
+			local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+			local clients = vim.lsp.get_active_clients()
+			if next(clients) == nil then
+				return msg
+			end
+			for _, client in ipairs(clients) do
+				local filetypes = client.config.filetypes
+				if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+					return '[' .. client.name .. ']'
+				end
+			end
+			return msg
+		end
 
-        require('lualine').setup {
-            options = {
-                globalstatus = true,
-                icons_enabled = true,
-                theme = custom_northern,
-                section_separators = {},
-                component_separators = "",
-                ignore_focus = {},
-                always_divide_middle = true,
-                refresh = {
-                    statusline = 1000,
-                    tabline = 1000,
-                    winbar = 1000,
-                },
-                disabled_filetypes = {
-                    statuslines = {},
-                    winbar = {},
-                    "alpha",
-                    "lazygit",
-                },
-            },
-            sections = {
-                lualine_a =
-                {
-                    {
-                        'mode',
-                        separator = {},
-                        right_padding = 2,
-                        left_padding = 2,
-                    },
-                    {
-                        'branch',
-                        icon = { '󰊢 ', color = { bg = colors.special, fg = colors.green }, align = 'left' },
-                        color = { bg = colors.special, fg = colors.yellow },
-                        separator = {},
-                    },
-                },
-                lualine_b = {
-                    {
-                        'diagnostics',
-                        sources = { 'nvim_lsp' },
-                        sections = { 'info' },
-                        diagnostics_color = { hint = { bg = colors.frostturquoise, fg = colors.dark } },
-                        update_in_insert = true,
-                        symbols = { info = ' ' },
-                    },
-                    {
-                        'diagnostics',
-                        sources = { 'nvim_lsp' },
-                        sections = { 'warn' },
-                        diagnostics_color = { warn = { bg = colors.yellow, fg = colors.dark } },
-                        update_in_insert = true,
-                        symbols = { warn = ' ' },
-                    },
-                    {
-                        'diagnostics',
-                        sources = { 'nvim_lsp' },
-                        sections = { 'error' },
-                        diagnostics_color = { error = { bg = colors.red, fg = colors.dark } },
-                        update_in_insert = true,
-                        symbols = { error = ' ' },
-                    },
-                },
-                lualine_c = {
-                    {
-                        'diagnostic-message',
-                        colors = {
-                            error = colors.red,
-                            warn = colors.yellow,
-                            info = colors.frostturquoise,
-                            hint = colors.frostturquoise,
-                        },
-                        icons = {
-                            error = "",
-                            warn = "",
-                            info = "",
-                            hint = "",
-                        },
-                    }
-                },
-                lualine_x = {
-                    {
-                        'diff',
-                        color = { bg = colors.dark }
-                    },
-                },
-                lualine_y = {
-                    {
-                        'filetype',
-                        color = { bg = colors.dark },
-                        icon_only = false,
-                        icon = { align = 'left' },
-                        colored = true,
-                    },
-                },
-                lualine_z = {
-                    {
-                        'searchcount',
-                        icon = { '', color = { fg = colors.dark }, align = 'left' },
-                    },
-                    {
-                        'selectioncount',
-                        icon = { '󰒅', color = { fg = colors.dark }, align = 'left' },
-                    },
-                    {
-                        'progress',
-                        left_padding = 2,
-                    }
-                },
-            },
-            inactive_sections = {
-                lualine_a = {},
-                lualine_b = {},
-                lualine_c = {},
-                lualine_x = {},
-                lualine_y = {},
-                lualine_z = {},
-            },
-            tabline = {},
-            winbar = {},
-            inactive_winbar = {},
-            extensions = {
-                'mason',
-                'neo-tree',
-                'nvim-dap-ui',
-                'quickfix',
-                'symbols-outline',
-                'trouble',
-            },
-        }
-    end
+		require('lualine').setup {
+			options = {
+				globalstatus = true,
+				icons_enabled = true,
+				theme = custom_northern,
+				section_separators = {},
+				component_separators = "",
+				ignore_focus = {},
+				always_divide_middle = true,
+				refresh = {
+					statusline = 1000,
+					tabline = 1000,
+					winbar = 1000,
+				},
+				disabled_filetypes = {
+					statuslines = {},
+					winbar = {},
+					"alpha",
+					"lazygit",
+				},
+			},
+			sections = {
+				lualine_a =
+				{
+					-- {
+					--     'mode',
+					--     separator = {},
+					--     right_padding = 2,
+					--     left_padding = 2,
+					-- },
+					{
+						'branch',
+						icon = { '󰊢 ', color = { bg = colors.special, fg = colors.green }, align = 'left' },
+						color = { bg = colors.special, fg = colors.yellow },
+						separator = {},
+					},
+					{
+						'diff',
+						color = { bg = colors.dark }
+					},
+				},
+				lualine_b = {
+				},
+				lualine_c = {
+				},
+				lualine_x = {
+					{
+						'diagnostics',
+						sources = { 'nvim_lsp' },
+						sections = { 'info' },
+						diagnostics_color = { hint = { bg = colors.frostturquoise, fg = colors.dark } },
+						update_in_insert = true,
+						symbols = { info = ' ' },
+					},
+					{
+						'diagnostics',
+						sources = { 'nvim_lsp' },
+						sections = { 'warn' },
+						diagnostics_color = { warn = { bg = colors.yellow, fg = colors.dark } },
+						update_in_insert = true,
+						symbols = { warn = ' ' },
+					},
+					{
+						'diagnostics',
+						sources = { 'nvim_lsp' },
+						sections = { 'error' },
+						diagnostics_color = { error = { bg = colors.red, fg = colors.dark } },
+						update_in_insert = true,
+						symbols = { error = ' ' },
+					},
+				},
+				lualine_y = {
+					{
+						lsp_server,
+						color = { bg = colors.dark },
+						icon = { '| 󰒋', color = { bg = colors.dark, fg = colors.frostturquoise }, align = 'left' },
+					},
+					{
+						'filetype',
+						color = { bg = colors.dark },
+						icon_only = false,
+						icon = { align = 'left' },
+						colored = true,
+					},
+				},
+				lualine_z = {
+					{
+						'searchcount',
+						icon = { '', color = { fg = colors.dark }, align = 'left' },
+					},
+					{
+						'selectioncount',
+						icon = { '󰒅', color = { fg = colors.dark }, align = 'left' },
+					},
+					-- {
+					--     'progress',
+					--     left_padding = 2,
+					-- }
+				},
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = {},
+			},
+			tabline = {},
+			winbar = {},
+			inactive_winbar = {},
+			extensions = {
+				'mason',
+				'neo-tree',
+				'nvim-dap-ui',
+				'quickfix',
+				'symbols-outline',
+				'trouble',
+			},
+		}
+	end
 }
