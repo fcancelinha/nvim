2fdc83d699d7f427a0af6caf256e11cae3d3eaab
Filipe Cancelinha <filipe.cancelinha@siemens.com>
2024-08-08 22:11:44 +0100

    config: improvement

--- init.lua
+++ init.lua
@@ -2,4 +2,10 @@ require('config.options')
 require('config.keymaps')
 require('config.autocommands')
 require('config.lazy')
-require('northern').load()
+
+local opts = {
+    italicized_comments = true,
+    brighter_comments = true,
+}
+
+require('northern').load(opts)
