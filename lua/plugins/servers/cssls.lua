b82ed081de1c484eae3a73c51f249d6539ad885d
Filipe Cancelinha <filipe.cancelinha@siemens.com>
2025-02-19 12:49:54 +0000

    config: improvements

--- lua/plugins/servers/cssls.lua
+++ lua/plugins/servers/cssls.lua
@@ -1,6 +1,1 @@
-return {
-  flags = {
-    debounce_text_changes = 150,
-    allow_incremental_sync = true,
-  }
-}
+return {}
--- lua/plugins/servers/lua_ls.lua
+++ lua/plugins/servers/lua_ls.lua
@@ -1,62 +1,62 @@
 return {
-  on_attach = function()
-    vim.api.nvim_create_autocmd('BufWritePre', {
-      pattern = '*.lua',
-      callback = function()
-        if vim.bo.buftype == '' then
-          vim.lsp.buf.format({ async = false })
-        end
-      end,
-    })
-  end,
-  settings = {
-    Lua = {
-      runtime = {
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        globals = {
-          'vim',
-          'use',
-          'describe',
-          'it',
-          'assert',
-          'before_each',
-          'after_each',
-        }
-      },
-      completion = {
-        enable = true,
-        callSnippet = 'Both',
-        keywordSnippet = 'Replace',
-      },
-      workspace = {
-        checkThirdParty = false,
-      },
-      telemetry = {
-        enable = false,
-      },
-      format = {
-        enable = true,
-        defaultConfig = {
-          quote_style = 'single',
-          indent_style = 'tab',
-          indent_size = '4',
+    on_attach = function()
+        vim.api.nvim_create_autocmd('BufWritePre', {
+            pattern = '*.lua',
+            callback = function()
+                if vim.bo.buftype == '' then
+                    vim.lsp.buf.format({ async = false })
+                end
+            end,
+        })
+    end,
+    settings = {
+        Lua = {
+            runtime = {
+                version = 'LuaJIT',
+            },
+            diagnostics = {
+                globals = {
+                    'vim',
+                    'use',
+                    'describe',
+                    'it',
+                    'assert',
+                    'before_each',
+                    'after_each',
+                }
+            },
+            completion = {
+                enable = true,
+                callSnippet = 'Both',
+                keywordSnippet = 'Replace',
+            },
+            workspace = {
+                checkThirdParty = false,
+            },
+            telemetry = {
+                enable = false,
+            },
+            format = {
+                enable = true,
+                defaultConfig = {
+                    quote_style = 'single',
+                    indent_style = 'tab',
+                    indent_size = '4',
+                },
+            },
+            lint = {
+                enable = true,
+                command = 'luacheck',
+                args = {
+                    '--globals',
+                    'vim',
+                    '--formatter',
+                    'plain',
+                    '--codes',
+                    '--ranges',
+                },
+            },
         },
-      },
-      lint = {
-        enable = true,
-        command = 'luacheck',
-        args = {
-          '--globals',
-          'vim',
-          '--formatter',
-          'plain',
-          '--codes',
-          '--ranges',
-        },
-      },
     },
-  },
 
 }
